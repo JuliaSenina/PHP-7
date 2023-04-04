2efd90cbaa0b2b60baa23ec607758e0405d103b5
Сенина Юлия Александровна <y.senina@ens.ru>
2023-04-04 08:18:56 +0300

    commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Test</title>
+</head>
+
+<body>
+<h1>Список страниц:</h1>
+  <ul>
+    <li><a href="onePage.php">Страница 1</a></li>
+    <li><a href="twoPage.php?text=Текст">Страница 2</a></li>
+    <li><a href="threePage.php">Страница 3</a></li>
+    <li><a href="fourPage.php">Страница 4</a></li>
+  </ul>
+</body>
+</html>
